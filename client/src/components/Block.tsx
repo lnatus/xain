a5bcec7a5d5367758920008e92a7466aaed63060
= <l.natus@live.com>
2018-04-30 15:31:51 +0200

    Switch block to styled components

--- client/src/components/Block.tsx
+++ client/src/components/Block.tsx
@@ -1,8 +1,41 @@
 import * as React from 'react'
 import Moment from 'react-moment'
+import styled from 'styled-components'
 
 import { Col, Row} from 'react-flexbox-grid'
-import '../css/block.css'
+
+const BlockStyled = styled.li`
+  padding: 15px;
+  margin-bottom: 15px;
+
+  background: #FFFFFF;
+  box-shadow: 0 1px 4px -1px rgba(0,0,0,0.50);
+
+  small {
+    float: right;
+  }
+
+  &.xain-genesis {
+    background: linear-gradient(-217deg, #67A0FA 2%, #8A51FF 96%);
+    color: #FFF;
+  }
+`
+
+const IndexStyled = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  line-height: 60px;
+  color: #FFF;
+  text-align: center;
+  background: linear-gradient(-217deg, #67A0FA 2%, #8A51FF 96%);
+  box-shadow: 0 1px 4px -1px rgba(0,0,0,0.50);
+
+  &.xain-genesis {
+    background: #FFF;
+    color: #8A51FF;
+  }
+`
 
 export interface IBlock {
   block: any
@@ -16,12 +49,12 @@ export default class Block extends React.Component<IBlock> {
   public render() {
     const block = this.props.block
     return (
-      <li className={'xain-block ' + (!block.index ? 'xain-genesis' : '') }>
+      <BlockStyled className={(!block.index ? 'xain-genesis' : '')}>
         <Row>
           <Col xs={12} md={12} lg={1}>
-            <div className="xain-block-index">
+            <IndexStyled className={(!block.index ? 'xain-genesis' : '')}>
               { block.index }
-            </div>
+            </IndexStyled>
           </Col>
           <Col xs={12} md={12} lg={11}>
           <div>
@@ -42,7 +75,7 @@ export default class Block extends React.Component<IBlock> {
           }
          </Col>
         </Row>
-      </li>
+      </BlockStyled>
     )
   }
 }
