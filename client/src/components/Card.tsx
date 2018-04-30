b09c9ba0327821db6fc80108bd5c669bcfedccc4
= <l.natus@live.com>
2018-04-30 15:08:50 +0200

    Switch card to style components

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
-import '../css/card.css'
+const CardStyled = styled.div`
+  padding: 15px;
+  margin: 15px 0;
+  box-shadow: 0 1px 4px -1px rgba(0,0,0,0.50);
+`
 
 export default class Card extends React.Component {
 
   public render () {
     const { children } = this.props
     return (
-      <div className="xain-card">
+      <CardStyled className="xain-card">
         { children }
-      </div>
+      </CardStyled>
     )
   }
 }
